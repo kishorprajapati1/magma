723705bdc339312632d36ae34f259852adf55d8b
kishorprajapati1 <kishorprajapati416@gmail.com>
2025-06-25 14:23:22 +0530

    delete

--- magma_v1.9_installer/04_run_orchestrator.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-echo "?? Running Magma Orchestrator using build.py..."
-
-cd /home/ubuntu/magma/orc8r/cloud/docker
-
-python3 build.py --all
-
-echo "? Magma Orchestrator is up!"
--- magma_v1.9_installer/05_run_metrics.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-echo "📊 Starting Orchestrator metrics services..."
-
-cd /home/ubuntu/magma/orc8r/cloud/docker
-python3 ./run.py --metrics
-
-echo "✅ Metrics services started!"
